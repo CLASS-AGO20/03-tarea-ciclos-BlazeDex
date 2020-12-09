065bd391349f256846520d67cad06e787483d877
Alan Hernández <ahernandez102@ucol.mx>
2020-12-08 18:13:28 -0600

    Updated sumatoriaSerieUno()

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 export default class App {
 sumatoriaSerieUno(numero) {
     let suma = 0;  
-    let variable = (1/numero);  
 
-    for(let i = 0; i <= 3.29; i = i + 1 + (1/2) + (1/3) + (1/4) + (1/5) + variable) {        
-        if(variable === (1/0)) {
-            suma = suma + i + 1 + (1/2) + (1/3) + (1/4) + (1/5) + variable;
-        } else {
-            suma = suma + i;
-        }
+    for(let i = 1; i <= numero; i = i + 1) {        
+        suma = suma + (1/i);
     }
     return suma;
 }
-   
+  
 }
 
 let app = new App();
-console.log(app.sumatoriaSerieUno(1000000000));
+console.log("Probando: sumatoriaSerieUno()");
+console.log(app.sumatoriaSerieUno(9));
+
