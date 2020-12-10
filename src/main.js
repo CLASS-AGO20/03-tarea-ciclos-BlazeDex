e69826c1bc73e7cef0df40f289d5af8d937eed7d
Alan Hernández <ahernandez102@ucol.mx>
2020-12-10 16:23:00 -0600

    Implemented esPrimo()

--- src/main.js
+++ src/main.js
@@ -24,16 +24,29 @@ sumatoriaSerieDos(numero) {
     return suma;
 }
     
-        
+esPrimo(numero) {
+    let i = numero;    
+    let tipo;
 
-  
+    do {
+        if((i % 2 === 0) && (i !== 2)|| (i === 1) || (i % (Math.sqrt(i)) === 0) || (i % 3 === 0) && (i !== 3) || (i % 5 === 0) && (i !== 5) || (i % 7 === 0) && (i !== 7) || (i % 13 === 0) && (i !== 13)) {
+            tipo = false;
+        } else {
+            tipo = true;
+        }
+        i++;
+    } while(i <= numero);
+    return tipo;
+ 
+}
+
+
 }
 
 let app = new App();
 console.log("Probando: sumatoriaSerieUno()");
-console.log(app.sumatoriaSerieUno(3));
+console.log(app.sumatoriaSerieUno(9));
 console.log("Probando: sumatoriaSerieDos()");
 console.log(app.sumatoriaSerieDos(7));
-
-
-
+console.log("Probando: esPrimo()");
+console.log(app.esPrimo(131));
